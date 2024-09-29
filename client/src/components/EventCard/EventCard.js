d6f83d4eff28ff4934852f62bc31f4824497245c
Abdul Mannan <mannan.jnv123@gmail.com>
2024-09-29 19:59:15 +0530

    update

--- client/src/components/EventCard/EventCard.js
+++ client/src/components/EventCard/EventCard.js
@@ -1,39 +1,32 @@
 "use client"
-import Link from "next/link";
-import React, { useState } from "react";
 import AnimatedElement from '@/components/Common/Animation/AnimatedElement'
-import { useAuth } from "@/context/auth";
-import ThreeDot from "./ThreeDot";
+import React, { useState } from "react";
+import Link from "next/link";
 
 export default function EventCard({ data }) {
 	const [ReadMore, setReadMore] = useState(false);
-	const { authUser, IsLoading, setAuthUser } = useAuth();
-
 	return (
 		<>
 			{/* Card  */}
-			<AnimatedElement>
-				<div className="rounded-lg min-h-96">
-					<img className="md:h-48 h-40 w-full rounded-lg object-cover object-center" src={data.posterURL} alt="img" />
+			<AnimatedElement >
+				<div className="h-full rounded-lg overflow-hidden">
+					<img className="md:h-48 h-40 w-full rounded-lg object-cover object-center" src={data.BannerPic} alt="img" />
 					<div className="p-2">
 						{/* <h2 className="tracking-widest text-xs title-font font-medium text-gray-400 mb-1">CATEGORY</h2> */}
-						<h1 className="title-font text-lg font-bold text-gray-900 mb-3">{data.title}</h1>
+						<h1 className="title-font text-lg font-bold text-gray-900 mb-3">{data.Title}</h1>
 						<p className="leading-relaxed text-sm sm:text-lg mb-3 text-gray-700">
-							{ReadMore ? data.details
+							{ReadMore ? data.Description
 								: <>
-									{data.details.substring(0, 100)} {data.details.length > 100 && "..."}
+									{data.Description.substring(0, 100)} {data.Description.length > 100 && "..."}
 								</>
 							}
 						</p>
-						<div className="flex justify-between">
-							{data.details.length > 100 &&
-								<div className="flex items-center flex-wrap ">
-									<button onClick={() => setReadMore(prev => !prev)} className="text-indigo-800 font-bold inline-flex items-center md:mb-2 lg:mb-0">{ReadMore ? "Read Less" : "Read More..."}
-									</button>
-								</div>
-							}
-							<ThreeDot id={data._id} />
-						</div>
+						{data.Description.length > 100 &&
+							<div className="flex items-center flex-wrap ">
+								<button onClick={() => setReadMore(prev => !prev)} className="text-indigo-800 font-bold inline-flex items-center md:mb-2 lg:mb-0">{ReadMore ? "Read Less" : "Read More..."}
+								</button>
+							</div>
+						}
 					</div>
 				</div>
 			</AnimatedElement>
